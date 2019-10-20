fe963dd319140f366b04dc3267cd64fc6cd8a919
lazarbogdan99 <48693214+lazarbogdan99@users.noreply.github.com>
2019-10-20 20:09:25 +0300

    Create MyShape.java

---
+++ MyShape.java
@@ -0,0 +1,5 @@
+triangle 250 250 3 -1 true 150 200 59 127 1000 true
+triangle 250 250 3 -1 true 120 205 60 126 1000 true
+triangle 150 150 6 1 false 80 199 61 128 1000 false
+triangle 100 100 4 1 false 60 150 62 125 1000 true
+triangle 300 300 3 -1 true 100 155 63 121 1000 true
