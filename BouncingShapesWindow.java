d7cf9a6b663f53fb9ecfc37e03c8a6b55c702e14
lazarbogdan99 <48693214+lazarbogdan99@users.noreply.github.com>
2019-10-20 20:08:07 +0300

    Create BouncingShapesWindow.java

---
+++ BouncingShapesWindow.java
@@ -0,0 +1,195 @@
+
+
+
+
+
+import java.util.ArrayList;
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
+public class BouncingShapesWindow {
+
+    private static final int ANIMATION_DELAY = 10;
+    private static final String FRAME_TITLE = "Shape Booooiiinggg Frame";
+
+    private GraphicsContext gc;
+    private Queue<ClosedShape> shapesToAdd;
+    private ArrayList<ClosedShape> activeShapes;
+    private int currentTime = 0;
+    private boolean flag=true;
+
+    private String filename;
+
+
+    public BouncingShapesWindow(GraphicsContext gc,String filename) {
+        this.gc=gc;
+
+        activeShapes=new ArrayList<ClosedShape>();
+        this.initShapes(filename);
+        this.insertShapes ();
+        drawClosedShapes();
+        actionPerformed();
+    }
+
+    private void drawClosedShapes () {
+        for (ClosedShape s : activeShapes)
+        {
+            s.draw(gc);
+        }
+    }
+
+    private void initShapes (String filename) {
+        shapesToAdd = ReadShapeFile.readDataFile(filename);
+    }
+
+    private void insertShapes() {
+        //no more shapes to add, we are done
+        if (shapesToAdd.isEmpty ()) {
+            return;
+        }
+
+        //add shapes if needed
+        ClosedShape current = shapesToAdd.peek ();
+        while (!shapesToAdd.isEmpty () && (current.getInsertionTime() <= currentTime*ANIMATION_DELAY)) {
+            activeShapes.add(current);
+            shapesToAdd.dequeue();
+            if (!shapesToAdd.isEmpty ()) {
+                current = shapesToAdd.peek();
+            }
+        }
+    }
+
+    public void actionPerformed()
+    {
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(5),ae -> onTime()));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+
+    }
+
+    private void onTime() {
+        currentTime++;
+        double h =gc.getCanvas().getHeight();
+        double w = gc.getCanvas().getWidth();
+        gc.clearRect(0, 0, w, h);
+        moveShapes();
+        insertShapes ();
+        drawClosedShapes();
+    }
+
+    /**
+     * this method decides if the shape has to pulse
+     * what shape is it
+     * seting the minimum and maximum size of pulsing
+     * when the shape reaches the max value, it starts to get smaller
+     * when the shape reaches the min value, it starts to get bigger
+     */
+    public void moveShapes()
+    {
+        double  dimsY = gc.getCanvas().getHeight() ;
+        double  dimsX = gc.getCanvas().getWidth() ;
+        for (ClosedShape s : activeShapes)
+        {
+            s.move();
+            if(s.isPulsing()){
+                if(s.getShape().equals("circle")){
+                    Circle circle=(Circle)s;
+                    if(circle.getDiameter() < 2.2*circle.getFirstDiameter() && circle.isMoving()) {
+                        circle.setDiameter(circle.getDiameter() + 1);
+                    }
+                    else if(circle.getDiameter() > circle.getFirstDiameter()) {
+                        circle.setDiameter(circle.getDiameter() - 1);
+                        circle.setMoving(false);
+                    }
+                    else{
+                        circle.setMoving(true);
+                    }
+                }
+                else if(s.getShape().equals("rect")){
+                    Rect rect = (Rect)s;
+                    if(rect.getHeight()*rect.getWidth() < 2.4*rect.getFirstHeight()*rect.getWidth() && rect.isMoving()){
+                        rect.setHeight(rect.getHeight() + 1);
+                        rect.setWidth(rect.getWidth()+1);
+                    }
+                    else if(rect.getHeight()*rect.getWidth()>rect.getFirstHeight()*rect.getWidth()) {
+                        rect.setHeight(rect.getHeight() - 1);
+                        rect.setWidth(rect.getWidth()-1);
+                        rect.setMoving(false);
+                    }
+                    else{
+                        rect.setMoving(true);
+                    }
+                }
+                else if(s.getShape().equals("oval")){
+                    Oval oval=(Oval) s;
+                    if(oval.getHeight()*oval.getWidth()<2.3*oval.getFirstHeight()*oval.getFirstWidth() && oval.isMoving()){
+                        oval.setHeight(oval.getHeight()+1);
+                        oval.setWidth(oval.getWidth()+1);
+                    }
+                    else if(oval.getHeight()*oval.getWidth()>oval.getFirstHeight()*oval.getFirstWidth()){
+                        oval.setHeight(oval.getHeight()-1);
+                        oval.setWidth(oval.getWidth()-1);
+                        oval.setMoving(false);
+                    }
+                    else{
+                        oval.setMoving(true);
+                    }
+                }
+                else if(s.getShape().equals("triangle")){
+                    Triangle triangle=(Triangle) s;
+                    if(triangle.getSide()<2.2*triangle.getFirstSide() && triangle.isMoving()) {
+                        triangle.setSide(triangle.getSide() + 1);
+                    }
+                    else if(triangle.getSide()>triangle.getFirstSide()){
+                        triangle.setSide(triangle.getSide()-1);
+                        triangle.setMoving(false);
+                    }
+                    else{
+                        triangle.setMoving(true);
+                    }
+                }
+                else if(s.getShape().equals("square")){
+                    Square square=(Square) s;
+                    if(square.getSide()<2.1*square.getFirstSide() && square.isMoving()) {
+                        square.setSide(square.getSide() + 1);
+                    }
+                    else if(square.getSide()>square.getFirstSide()){
+                        square.setSide(square.getSide()-1);
+                        square.setMoving(false);
+                    }
+                    else{
+                        square.setMoving(true);
+                    }
+                }
+            }
+
+            // Move us back in and bounce if we went outside the drawing area.
+            if (s.outOfBoundsX(dimsX))
+            {
+                s.putInBoundsX(dimsX);
+                s.bounceX();
+            }
+
+            if (s.outOfBoundsY(dimsY))
+            {
+                s.putInBoundsY(dimsY);
+                s.bounceY();
+            }
+
+        }
+    }
+
+}
